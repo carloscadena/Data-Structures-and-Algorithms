dca749ef680f1464a8055de2a1ba7ac2c2bd8c57
carlos cadena <carloscadena@live.com>
2018-10-10 11:10:57 -0700

    Implementation of array shift

---
+++ Challenges/ArrayShift/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ArrayShift();
+        }
+
+        static void ArrayShift(int[] arr, int num)
+        {
+            int[] newArr = new int[arr.Length + 1];
+            double middle = Math.Ceiling(arr.Length / 2);
+            for(int i = 0; i < newArr.Length; i++)
+            {
+                if(i < middle)
+                {
+                    newArr[i] == arr[i];
+                }
+                else if (i == middle)
+                {
+                    newArr[i] == num;
+                }
+                else
+                {
+                    newArr[i] == arr[i - 1];
+                }
+            }
+            return newArr;
+        }
+    }
+}
