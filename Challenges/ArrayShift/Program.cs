e2899577d1bff79123a1b815a1934c14056a3664
carlos cadena <carloscadena@live.com>
2018-10-10 11:40:19 -0700

    fixed arrayshift method

--- Challenges/ArrayShift/Program.cs
+++ Challenges/ArrayShift/Program.cs
@@ -6,27 +6,27 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            ArrayShift();
+            int[] testArr = { 23, 44, 55, 12, 21, 5 };
+            ArrayShift(testArr, 10);
         }
 
-        static void ArrayShift(int[] arr, int num)
+        static int[] ArrayShift(int[] arr, int num)
         {
             int[] newArr = new int[arr.Length + 1];
-            double middle = Math.Ceiling(arr.Length / 2);
+            double middle = Math.Ceiling((double) arr.Length / 2);
             for(int i = 0; i < newArr.Length; i++)
             {
                 if(i < middle)
                 {
-                    newArr[i] == arr[i];
+                    newArr[i] = arr[i];
                 }
                 else if (i == middle)
                 {
-                    newArr[i] == num;
+                    newArr[i] = num;
                 }
                 else
                 {
-                    newArr[i] == arr[i - 1];
+                    newArr[i] = arr[i - 1];
                 }
             }
             return newArr;
