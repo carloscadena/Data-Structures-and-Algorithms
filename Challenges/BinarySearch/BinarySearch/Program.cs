f8121d65926e14edea950d99cc801d1428a17d68
Carlos Cadena <cs.cadena@gmail.com>
2018-10-11 11:40:28 -0700

    added BS call to main method

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -7,7 +7,8 @@ namespace BinarySearch
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //BinarySearch();
+            int[] temp = { 1, 2, 3, 4, 5, 6 };
+            BinarySearch(temp, 5);
         }
         public static int BinarySearch(int[] arr, int num)
         {
