bbcad56ce97db7b10f7222fb08c758eb5bac0ac8
carlos cadena <carloscadena@live.com>
2018-10-22 09:48:33 -0700

    implementation of stacks and queues

---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        // Peek at Node on the front of queue
+        public Node Peek()
+        {
+            return Front;
+        }
+
+        // Enqueue adds new node to end of queue
+        public void Enqueue(Node node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        // Dequeue removes node from front and makes next node the front
+        public Node Dequeue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+    }
+}
