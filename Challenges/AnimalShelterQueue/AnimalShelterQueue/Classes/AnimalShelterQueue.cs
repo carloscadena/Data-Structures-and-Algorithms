7022de39bf6d62194f6eee1f77352cd124c9c25e
carlos cadena <carloscadena@live.com>
2018-10-24 11:35:56 -0700

    needs work

---
+++ Challenges/AnimalShelterQueue/AnimalShelterQueue/Classes/AnimalShelterQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StacksAndQueues.Classes;
+
+namespace AnimalShelterQueue.Classes
+{
+    public class AnimalShelterQueue
+    {
+        Queue q = new Queue(new Node("marker"));
+
+        public void Enqueue(Node animal)
+        {
+            q.Enqueue(animal);
+        }
+        public void Dequeue(string pref)
+        {
+            q.Enqueue(q.Dequeue());
+            while ((string)q.Front.Value != "marker")
+            {
+                if(q.Front.Value == )
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Challenges/AnimalShelterQueue/AnimalShelterQueue/Classes/Cat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalShelterQueue.Classes
+{
+    class Cat : Animal
+    {
+    }
+}
