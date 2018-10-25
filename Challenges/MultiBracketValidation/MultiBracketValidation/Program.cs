eb3023719aa54660660c698055e7ffd06766bc63
carlos cadena <carloscadena@live.com>
2018-10-25 10:17:05 -0700

    initial setup and commit

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StacksAndQueues.Classes;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool MultiBracketValidation()
+        {
+
+        }
+    }
+}
