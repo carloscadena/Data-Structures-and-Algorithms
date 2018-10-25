82350d2c35edc729c54be5a7f77d0b7d6bc5af0d
carlos cadena <carloscadena@live.com>
2018-10-25 10:52:51 -0700

    working multibracketvalidation method

--- Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -8,10 +8,42 @@ namespace MultiBracketValidation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MultiBracketValidation("(()})");
         }
 
-        static bool MultiBracketValidation()
+        static bool MultiBracketValidation(string brackets)
         {
+            char[] charArr = brackets.ToCharArray();
+            Stack bracketStack = new Stack(null);
+            string openBrackets = "([{";
+            string closeBrackets = ")]}";
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if (openBrackets.Contains(charArr[i]))
+                {
+                    Console.WriteLine("open brack");
+                    bracketStack.Push(new Node(charArr[i]));
+                }
+                if (closeBrackets.Contains(charArr[i]))
+                {
+                    string temp = bracketStack.Pop().Value.ToString();
+                    if (charArr[i].ToString() == ")")
+                    {
+                        if (temp != "(") return false;
+                    }
+                    if (charArr[i].ToString() == "]")
+                    {
+                        if (temp != "[") return false;
+                    }
+                    if (charArr[i].ToString() == "}")
+                    {
+                        if (temp != "{") return false;
+                    }
+                }
+
+            }
+            if (bracketStack.Top == null) return true;
+            else return false;
 
         }
     }
