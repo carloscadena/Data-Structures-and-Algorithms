8f14add4eaef09a4aa8c2f62348a9d496e2b9a18
Carlos Cadena <cs.cadena@gmail.com>
2018-11-18 12:17:22 -0800

    initial commit

---
+++ Challenges/MostCommonWord/MostCommonWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MostCommonWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
